1d633d7c4e248ad00321ed1017efbab27841ec67
brendan <hbrovholo@gmail.com>
2025-12-18 08:24:12 +0200

    make app run

--- src/main/java/com/bitkulcha/notification_engine/config/FirebaseConfig.java
+++ src/main/java/com/bitkulcha/notification_engine/config/FirebaseConfig.java
@@ -6,24 +6,30 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import java.io.InputStream;
+import org.springframework.context.annotation.Profile;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
+
+    @Value("${firebase.credential}")
+    private String firebaseCredential;
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("homeq-d1e12-firebase-adminsdk-fbsvc-5e042a9a3b.json").getInputStream();
+        GoogleCredentials credentials = GoogleCredentials.fromStream( new ByteArrayInputStream(firebaseCredential.getBytes()) );
 
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(credentials)
                 .build();
 
-        if (FirebaseApp.getApps().isEmpty()) { // Check if the app is already initialized
+        if (FirebaseApp.getApps().isEmpty()) {
             return FirebaseApp.initializeApp(options);
         }
         return FirebaseApp.getInstance();
--- src/test/java/com/bitkulcha/notification_engine/NotificationEngineApplicationTests.java
+++ src/test/java/com/bitkulcha/notification_engine/NotificationEngineApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.profiles.active=test")
 class NotificationEngineApplicationTests {
 
 	@MockitoBean
